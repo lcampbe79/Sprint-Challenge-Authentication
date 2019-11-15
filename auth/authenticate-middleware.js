696f96a3f7a722c8004c9e5aca1138ad10f67765
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-11-15 09:00:45 -0800

    Authentication middleware works, returns jokes.

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,5 +1,21 @@
-
+const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  const token = req.headers.authorization
+
+  if (token) {
+    const secret = process.env._JWT_SECRET || 'secrets are fun'
+
+    jwt.verify(token, secret, (error, decodedToken) => {
+      if (error) {
+        res.status(403).json({message: 'Invalid Credentials'})
+      } else {
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
