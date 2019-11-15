aabd5879b1743e663dd2b926002ac22c8e683a89
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-11-15 08:51:29 -0800

    Login works and send tokens.

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -1,29 +1,30 @@
-const db = require('../database/dbConfig');
+
+const db = require('../database/dbConfig.js');
 
 module.exports = {
+  add,
+  find,
   findBy,
   findById,
-  add
+};
+
+function find() {
+  return db('users').select('id', 'username', );
 }
 
 function findBy(filter) {
-  return db('users')
-    // .select('id', 'username', 'password')
-    .where(filter);
+ 
+  return db('users').where(filter);
+}
+
+async function add(user) {
+  const [id] = await db('users').insert(user);
+
+  return findById(id);
 }
 
 function findById(id) {
   return db('users')
-    .select('id', 'username')
     .where({ id })
     .first();
-}
-
-function add(user) {
-  return db('users')
-    .insert(user, 'id')
-    .then(ids => {
-      const [id] = ids;
-      return findById(id);
-    });
 }
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -9,6 +9,11 @@ const Users = require('../auth/auth-model');
 router.post('/register', (req, res) => {
   let user = req.body;
 
+  if (!user.username || !user.password) {
+    res.status(400).json({message: 'Sorry, please enter required information'})
+    return
+  } 
+
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
 
@@ -22,9 +27,36 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let {username, password} = req.body;
+
+  Users.findBy({username})
+    .first()
+    .then(user => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+
+        res.status(200).json({
+          message: `Welcome ${user.username}! Please take this token...`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 });
 
 module.exports = router;
 
-
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const secret = process.env.JWT_SECRET || 'secrets are fun'
+  const options = {
+    expiresIn:'1d'
+  }
+  return jwt.sign(payload, secret, options)
+}
